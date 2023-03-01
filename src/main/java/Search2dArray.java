c59a9ea68381cbd8692f2b75e3955d81a008cd1e
ASOUDRY <soudryalex@gmail.com>
2023-03-01 22:48:25 +0000

    done

--- src/main/java/Search2dArray.java
+++ src/main/java/Search2dArray.java
@@ -10,6 +10,13 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+        for (int[] item: nums) {
+            for (int subItem: item) {
+                if (subItem == target) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
